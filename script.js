b2fe957640c90f30d6a79e86f7b255f6c86e3429
Aramez <191219550+Aramez-0@users.noreply.github.com>
2025-01-24 04:00:31 +1000

    Added basic functionality

--- script.js
+++ script.js
@@ -11,39 +11,116 @@ let zero = document.querySelector("#zero");
 let point = document.querySelector("#point");
 let divide = document.querySelector("#divide");
 let multiply = document.querySelector("#multiply");
-let addition = document.querySelector("#addition");
-let subtraction = document.querySelector("#subtraction");
+let add = document.querySelector("#add");
+let subtract = document.querySelector("#subtract");
+let clear = document.querySelector("#clear")
 let equals = document.querySelector("#equals");
-let display = document.querySelector("#display")
+let display = document.querySelector("#display");
+let input = document.querySelector("#input")
+let tgRnd = document.querySelector("#toggle-rounding"); 
+let sum = {num1: '', operator: null, num2: ''};
+let rounding = true;
+let newEquation = false;
+
+tgRnd.addEventListener("click", () => {
+    if (rounding === true) {
+        tgRnd.textContent = "Toggle rounded numbers: Off";
+        rounding = false;
+    } else {
+        tgRnd.textContent = "Toggle rounded numbers: On";
+        rounding = true;
+    };
+});
+
+function calculating() {
+    let num1 = parseFloat(sum.num1);
+    let operator = sum.operator;
+    let num2 = parseFloat(sum.num2);
+    if (operator === "+") {
+        return addition(num1, num2);
+    } else if (operator === "-") {
+        return subtraction(num1, num2);
+    } else if (operator === "*") {
+        return multiplication(num1, num2);
+    } else if (operator === "/") {
+        return division(num1, num2);
+    } else {
+        return "This does not work";
+    };
+};
+
+function addition(a, b) {
+    if (rounding === true) {
+        return Math.round(a + b);
+    } else return a + b;
+};
+function subtraction(a, b) {
+    if (rounding === true) {
+        return Math.round(a - b);
+    } else return a - b;
+};
+function multiplication(a, b) {
+    if (rounding === true) {
+        return Math.round(a * b);
+    } else return a * b;
+};
+function division(a, b) {
+    if (b !== 0 && rounding === true) {
+        return Math.round(a / b);
+    } else if (b !== 0) {
+        return a / b
+    } else return 0;
+};
 
 function btn(a, b) {
     a.addEventListener("click", () => {
-        console.log(b);
-        if (b === "/" || b === "*" || b=== "+" || b === "-") {
-            display.textContent += ` ${b} `
+        if (newEquation === true) {
+            display.value = '';
+            newEquation = false;
+        };
+        if (b === "/" || b === "*" || b === "+" || b === "-") {
+            display.value += ` ${b} `;
+            sum.operator = b;
+            point.disabled = false
+        } else if (b === "clr") {
+            display.value = '';
+            sum = {num1: '', operator: null, num2: ''};
+            point.disabled = false;
         } else if (b === "=") {
-
+            let result = calculating();
+            display.value = result;
+            sum = {num1: '', operator: null, num2: ''};
+            point.disabled = false;
+            newEquation = true;
+        } else if (sum.operator === null) {
+            display.value += b;
+            sum.num1 += b;
         } else {
-            display.textContent += b
-        }
-        return b;
+            display.value += b;
+            sum.num2 += b;
+        };
+        if (b === ".") {
+            point.disabled = true;
+        };
     });
 };
 
-btn(nine, "9");
-btn(eight, "8");
-btn(seven, "7");
-btn(six, "6");
-btn(five, "5");
-btn(four, "4");
-btn(three, "3");
-btn(two, "2");
-btn(one, "1");
-btn(zero, "0");
+btn(nine, '9');
+btn(eight, '8');
+btn(seven, '7');
+btn(six, '6');
+btn(five, '5');
+btn(four, '4');
+btn(three, '3');
+btn(two, '2');
+btn(one, '1');
+btn(zero, '0');
 btn(point, ".");
 btn(divide, "/");
 btn(multiply, "*");
-btn(addition, "+");
-btn(subtraction, "-");
+btn(add, "+");
+btn(subtract, "-");
+btn(clear, "clr")
 btn(equals, "=");
 
+// should handle multiple parameters
